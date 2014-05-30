f38b6519f76b04a2a96dcd5d2d982f9628165e04
halgatewood <halgatewood@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-30 20:43:32 +0000

    Minor updates 2.0.3
    git-svn-id: https://plugins.svn.wordpress.org/testimonial-rotator/tags/2.0.3@923992 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/single-testimonial.php
@@ -0,0 +1,65 @@
+<?php
+
+global $post;
+
+$rotator_id 		= get_post_meta( get_the_ID(), '_rotator_id', true );
+$itemreviewed 		= get_post_meta( $rotator_id, '_itemreviewed', true );
+$cite 				= get_post_meta( get_the_ID(), '_cite', true );
+$rating 			= (int) get_post_meta( get_the_ID(), '_rating', true );
+$has_image 			= has_post_thumbnail() ? "has-image" : false;
+
+// WRAPPER
+echo "<div class=\"testimonial_rotator_single hreview itemreviewed item {$has_image} cf-tr\">\n";		
+
+// POST THUMBNAIL
+if ( $has_image )
+{ 
+	echo "	<div class=\"testimonial_rotator_img img\">" . get_the_post_thumbnail( get_the_ID(), $img_size ) . "</div>\n"; 
+}
+
+// DESCRIPTION
+echo "	<div class=\"text testimonial_rotator_description\">\n";
+
+// RATING
+if($rating)
+{
+	echo "<div class=\"testimonial_rotator_stars cf-tr\">\n";
+	for($r=1; $r <= $rating; $r++)
+	{
+		echo "	<div class=\"testimonial_rotator_star testimonial_rotator_star_$r\"><i class=\"fa fa-star\"></i></div>";
+	}
+	echo "</div>\n";
+}
+
+// CONTENT
+echo "<div class=\"testimonial_rotator_quote\">\n";
+echo get_the_content();
+echo "</div>\n";
+
+// AUTHOR INFO
+if( $cite )
+{
+	echo "<div class=\"testimonial_rotator_author_info cf-tr\">\n";
+	echo wpautop($cite);
+	echo "</div>\n";				
+}
+
+echo "	</div>\n";
+	
+// MICRODATA
+echo "	<div class=\"testimonial_rotator_microdata\">\n";
+
+	if($itemreviewed) echo "\t<div class=\"fn\">{$itemreviewed}</div>\n";
+	if($rating) echo "\t<div class=\"rating\">{$rating}.0</div>\n";
+	
+	echo "	<div class=\"dtreviewed\"> " . get_the_date('c') . "</div>";
+	echo "	<div class=\"reviewer\"> ";
+		echo "	<div class=\"fn\"> " . wpautop($cite) . "</div>";
+		if ( has_post_thumbnail() ) { echo get_the_post_thumbnail( get_the_ID(), 'thumbnail', array('class' => 'photo' )); }
+	echo "	</div>";
+	echo "	<div class=\"summary\"> " . $post->post_excerpt . "</div>";
+	echo "	<div class=\"permalink\"> " . get_permalink() . "</div>";
+
+echo "	</div> <!-- .testimonial_rotator_microdata -->\n";
+
+echo "</div>\n";
